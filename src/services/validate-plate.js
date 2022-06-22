70990f051aee1939ca540a1e5e20525b47161b66
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-06-22 09:40:35 -0500

    iterations 2,3,4

--- src/services/validate-plate.js
+++ src/services/validate-plate.js
@@ -1,4 +1,11 @@
 export const isValidPlate = (str) => {
-  if (str === "" || str.length > 7)
+  if (str === "" || str.length > 7) {
     return { isValid: false, letters: null, numbers: null };
+  }
+  const letters = str.substr(0, 3);
+  const numbers = str.substr(3).split('').map(x=>+x);
+  if(numbers.includes(NaN)) return { isValid: false, letters: null, numbers: null }
+  if(!/^[a-zA-Z]+$/.test(letters)) return { isValid: false, letters: null, numbers: null }
+
+  return { isValid: true, letters, numbers };
 };
--- src/services/validate-plate.test.js
+++ src/services/validate-plate.test.js
@@ -7,11 +7,26 @@ describe("isValidPlate - basic functionality", () => {
   it("Return response.isValid : false when passed an empty string", () => {
     const expected = { isValid: false, letters: null, numbers: null };
     const actual = isValidPlate("");
-    expect(actual).to.deep.equal(expected)
+    expect(actual).to.deep.equal(expected);
   });
   it("Return response.isValid : false when the string has more than 7 chars", () => {
     const expected = { isValid: false, letters: null, numbers: null };
     const actual = isValidPlate("AAAA12345");
-    expect(actual).to.deep.equal(expected)
+    expect(actual).to.deep.equal(expected);
+  });
+  it("Return letters and numbers if correct size", () => {
+    const expected = { isValid: true, letters: "AAA", numbers: [1, 2, 3, 4] };
+    const actual = isValidPlate("AAA1234");
+    expect(actual).to.deep.equal(expected);
+  });
+  it("Return response.isValid : false if it has Letters in numbers", () => {
+    const expected = { isValid: false, letters: null, numbers: null };
+    const actual = isValidPlate("AAA123A");
+    expect(actual).to.deep.equal(expected);
+  });
+  it("Return response.isValid : false if it has numbers in letters", () => {
+    const expected = { isValid: false, letters: null, numbers: null };
+    const actual = isValidPlate("A1A1234");
+    expect(actual).to.deep.equal(expected);
   });
 });
