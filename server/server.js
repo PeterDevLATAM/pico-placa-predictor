32da7144d3c0cb3b27988a357386e50e350e155f
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-06-23 14:17:28 -0500

    basic server and fetch data in App

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const  {DATA} = require('./data')
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+console.log(DATA)
+app.get('/api/regulations', (req, res) => {
+  res.send(DATA);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import Home from './pages/home.component.jsx';
-import "./sass/_variables.scss"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Home from "./pages/home.component.jsx";
+import "./sass/_variables.scss";
 
 function App() {
+  const [state, setState] = useState({});
+  useEffect(() => {
+    fetch("http://localhost:3001/api/regulations")
+      .then((res) => res.json())
+      .then((res) => setState(res));
+  }, []);
+  console.log(state);
   return (
     <div className="App">
-      <Home/>
+      <Home />
     </div>
   );
 }
