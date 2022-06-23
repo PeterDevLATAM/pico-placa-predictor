403b9bbb830926a28a1331c39b0162d584731d61
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-06-23 14:56:35 -0500

    regulations redux store

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { queryReducer } from "./query/query.reducer.js";
+import { regulationsReducer } from "./regulations/regulations.reducer.js";
 
 export const rootReducer = combineReducers({
   query: queryReducer,
+  regulations: regulationsReducer,
 });
