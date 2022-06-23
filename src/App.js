6ddc7f60bc6e863ecb17a51b85b127bea1c668c6
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-06-23 17:27:44 -0500

    delete unused vars, fixing scss warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./App.css";
 import Home from "./pages/home.component.jsx";
@@ -6,12 +6,14 @@ import "./sass/_variables.scss";
 import { setRegulations } from "./store/regulations/regulations.actions.js";
 
 function App() {
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     fetch("http://localhost:3001/api/regulations")
       .then((res) => res.json())
       .then((res) => dispatch(setRegulations(res)));
-  }, []);
+  }, [dispatch]);
   return (
     <div className="App">
       <Home />
